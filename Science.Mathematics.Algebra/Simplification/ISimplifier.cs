f29ad251978ccf24e782ff15cdac78d766f5ced6
Peter-Juhasz <peter.juhasz@live.com>
2015-01-31 18:14:24 +0100

    Scalable infrastructure for expression simplification.

---
+++ Science.Mathematics.Algebra/Simplification/ISimplifier.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading;
+
+namespace Science.Mathematics.Algebra.Simplification
+{
+    /// <summary>
+    /// Provides simplification logic for a given type of <see cref="AlgebraExpression"/>.
+    /// </summary>
+    /// <typeparam name="TExpression"></typeparam>
+    public interface ISimplifier<TExpression> where TExpression : AlgebraExpression
+    {
+        /// <summary>
+        /// Simplifies an <see cref="AlgebraExpression"/> of the desired kind.
+        /// </summary>
+        /// <param name="expression"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns>The simplified expression.</returns>
+        AlgebraExpression Simplify(TExpression expression, CancellationToken cancellationToken);
+    }
+}
---
+++ Science.Mathematics.Algebra/Simplification/Simplifier.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+
+namespace Science.Mathematics.Algebra.Simplification
+{
+    /// <summary>
+    /// Simplifies <see cref="AlgebraExpression"/>s using all the simplifiers found in the library.
+    /// </summary>
+    public static class Simplifier
+    {
+        /// <summary>
+        /// Simplifies an <see cref="AlgebraExpression"/> as much as it can.
+        /// </summary>
+        /// <param name="expression"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public static AlgebraExpression Simplify(AlgebraExpression expression, CancellationToken cancellationToken)
+        {
+            AlgebraExpression simplified = expression;
+            AlgebraExpression lastResult;
+
+            do
+            {
+                lastResult = simplified;
+
+                // simplify by kind
+                simplified = SimplifySpecificKind(simplified, cancellationToken);
+            } while (simplified != lastResult);
+
+            return simplified;
+        }
+
+        private static AlgebraExpression SimplifySpecificKind(AlgebraExpression expression, CancellationToken cancellationToken)
+        {
+            AlgebraExpression simplified = expression;
+            AlgebraExpression lastResult;
+
+            // find applicable simplifiers
+            IReadOnlyCollection<object> simplifiers = FindSimplifiers(expression.GetType())
+                .Select(Activator.CreateInstance)
+                .ToList()
+            ;
+
+            do
+            {
+                lastResult = simplified;
+
+                // query each simplifier
+                foreach (var simplifier in simplifiers)
+                {
+                    cancellationToken.ThrowIfCancellationRequested();
+
+                    MethodInfo method = simplifier.GetType().GetRuntimeMethod("Simplify", new[] { expression.GetType(), typeof(CancellationToken) });
+                    simplified = method.Invoke(null, new[] { simplified }) as AlgebraExpression;
+
+                    // stop if not the same kind of expression
+                    if (lastResult.GetType() != simplified.GetType())
+                        return simplified;
+                }
+            } while (simplified != lastResult);
+
+            return simplified;
+        }
+
+        private static IEnumerable<Type> FindSimplifiers(Type expressionType)
+        {
+            var interfaceType = typeof(ISimplifier<>).MakeGenericType(expressionType);
+
+            return typeof(ISimplifier<>).GetTypeInfo().Assembly.ExportedTypes
+                .Where(t => t.GetTypeInfo().ImplementedInterfaces.Contains(interfaceType))
+            ;
+        }
+    }
+}
