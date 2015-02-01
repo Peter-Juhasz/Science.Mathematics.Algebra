516fcbbc47cf11320a59e116eb0a4bf4d31999b5
Peter-Juhasz <peter.juhasz@live.com>
2015-02-01 21:53:28 +0100

    Constant simplifier

---
+++ Science.Mathematics.Algebra/Simplification/ConstantSimplifier.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading;
+
+namespace Science.Mathematics.Algebra.Simplification
+{
+    /// <summary>
+    /// Simplifies expressions to their constant value, if possible.
+    /// </summary>
+    public sealed class ConstantSimplifier : ISimplifier<AlgebraExpression>
+    {
+        public AlgebraExpression Simplify(AlgebraExpression expression, CancellationToken cancellationToken)
+        {
+            double? constantValue = expression.GetConstantValue();
+
+            if (constantValue != null)
+                return ExpressionFactory.Constant(constantValue.Value);
+
+            return expression;            
+        }
+    }
+}
