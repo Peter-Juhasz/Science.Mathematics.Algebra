e00c0a88fd4b9f3ca84f96dd9a41916d8db88e2c
Peter-Juhasz <peter.juhasz@live.com>
2016-11-29 08:36:24 +0100

    Simplifiers

---
+++ src/Science.Mathematics.Algebra/Simplification/AbsoluteValue/NestedAbsoluteValueSimplifier.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading;
+
+namespace Science.Mathematics.Algebra
+{
+    /// <summary>
+    /// Simplifies expressions.
+    /// </summary>
+    internal sealed class NestedAbsoluteValueSimplifier : ISimplifier<AbsoluteValueExpression>
+    {
+        public AlgebraExpression Simplify(AbsoluteValueExpression expression, CancellationToken cancellationToken)
+        {
+            return expression.WithExpression(expression.Expression.Simplify());
+        }
+    }
+}
